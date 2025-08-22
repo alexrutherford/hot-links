2f4931ef285fc1b327f4df47c9f3d2078efcc977
Alex Rutherford <alexisadams@gmail.com>
2025-08-22 19:49:23 +0000

    Persist links as JSON

--- data/make_data.py
+++ data/make_data.py
@@ -1,1 +1,37 @@
-# port over code from grabbing data from Kaggle API
+import re
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def is_article(url):
+	if re.match(r'https://www.theguardian.com/', url):
+		return True
+	return False
+
+def save_links(blob):
+    with open('../data/external/{:s}.json'.format(blob['id']), 'a') as f:
+        json.dump(blob, f)
+
+
+def get_links(blob):
+	url = blob['webUrl']
+	id = str(blob['id'])
+    
+	print('URL', url)
+	print('ID', id)
+    
+	r = requests.get(url)
+	soup = BeautifulSoup(r.content, 'html.parser')
+	res = {}
+	res['url'] = url
+	res['id'] = id
+	res['links'] = []
+    
+	main = soup.find('main')
+	if main:
+		soup = main
+
+	for a in soup.find_all('a', href=True):
+		res['links'].append({'href': a['href'], 'link': a.get_text(strip=True)})
+
+	return res
