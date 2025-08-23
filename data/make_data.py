e8fa0740befad7d3d34b4160143555ce7cb0dd84
Alex Rutherford <alexisadams@gmail.com>
2025-08-23 07:04:42 +0000

    Set up logging

--- data/make_data.py
+++ data/make_data.py
@@ -1,12 +1,27 @@
 import re
+import sys
 import requests
 from bs4 import BeautifulSoup
 import json
+import logging
 
-def is_article(url):
-	if re.match(r'https://www.theguardian.com/', url):
-		return True
-	return False
+logging.basicConfig(filename='example.log', filemode='a', level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+def is_article(url: str) -> bool:
+    """
+    Checks if the given URL is an article from The Guardian and not the home page etc
+
+    Args:
+        url (str): The URL to check.
+
+    Returns:
+        bool: True if the URL starts with 'https://www.theguardian.com/', False otherwise.
+    """
+    if re.match(r'https://www.theguardian.com/', url):
+        return True
+    return False
 
 def save_links(blob):
     with open('../data/external/{:s}.json'.format(blob['id']), 'a') as f:
@@ -14,24 +29,24 @@ def save_links(blob):
 
 
 def get_links(blob):
-	url = blob['webUrl']
-	id = str(blob['id'])
-    
-	print('URL', url)
-	print('ID', id)
-    
-	r = requests.get(url)
-	soup = BeautifulSoup(r.content, 'html.parser')
-	res = {}
-	res['url'] = url
-	res['id'] = id
-	res['links'] = []
-    
-	main = soup.find('main')
-	if main:
-		soup = main
+    url = blob['webUrl']
+    id = str(blob['id'])
 
-	for a in soup.find_all('a', href=True):
-		res['links'].append({'href': a['href'], 'link': a.get_text(strip=True)})
+    logging.debug('URL: {:s}'.format(url))
+    logging.debug('ID: {:s}'.format( id))
 
-	return res
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    res = {}
+    res['url'] = url
+    res['id'] = id
+    res['links'] = []
+    
+    main = soup.find('main')
+    if main:
+        soup = main
+
+    for a in soup.find_all('a', href=True):
+        res['links'].append({'href': a['href'], 'link': a.get_text(strip=True)})
+
+    return res
